a24a73e5c7bdd0c51d48eb7f59b43ca52b52d4d8
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2017-02-10 12:07:21 +0000

    .

--- cnp_news/cnp_news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from letter.urls import *
-
-urlpatterns = patterns('',
-    url(r'^', include('letter.urls')),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
